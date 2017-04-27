3dc5afd0b93a69034fcd2f5b55c8bd7126cf28d4
luide <luide@luide-HP-G42-Notebook-PC>
2017-04-27 17:24:07 -0300

    Este é o meu segundo commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h4>Teste</h4>
+        <?php
+       /* $variavel = array(
+	"nome" => "lucas",
+	"sobrenome"=> "mendes",
+	"idade" => 25,
+	);
+       print_r($variavel);
+       //echo $variavel ["nome"]; */
+        $produtos = array(
+	0 => array(
+		"nome"=> "",
+		"quantidade" => "",
+		"info" => ""
+		),
+	1 => array(
+		"nome"=> "",
+		"quantidade" => "",
+		"info" => ""
+		),
+	);
+        print_r($produtos);
+        ?>
+    </body>
+</html>
